74545fb49a9226fa0230426be52ba488595a2113
Grégory Grzesiak <gregory@grzesiak.fr>
2024-04-08 13:12:01 +0200

    Add Objet Point

--- Optic Geometric/systemecentrecalculatrice.py
+++ Optic Geometric/systemecentrecalculatrice.py
@@ -55,6 +55,10 @@ class systemecentree():
         for i in range(len(self.list_of_matrice)-1):
             self.list_of_matrice[0] = self.multiply_matrice_two_by_two(self.list_of_matrice[0], self.list_of_matrice[i+1])
         
+        for i in range(len(self.list_of_matrice[0])):
+            for j in range(len(self.list_of_matrice[0][i])):
+                self.list_of_matrice[0][i][j] = round(self.list_of_matrice[0][i][j],3)
+             
     def calc_and_print_result(self):
 
         indice_before = self.list_of_indice[0]
@@ -62,26 +66,65 @@ class systemecentree():
         indice_after = self.list_of_indice[-1]
         
         self.matrice_transfert = self.list_of_matrice[0]
-        self.fo = round(-indice_before/(-self.matrice_transfert[1][0]),2)
-        self.fi = round(indice_after/(-self.matrice_transfert[1][0]),2)
-        self.SHi = round(self.fi*(self.matrice_transfert[0][0]-1),2)
-        self.EHo = round(self.fo*(self.matrice_transfert[1][1]-1),2)
-        self.ENo = round(self.fo*(self.matrice_transfert[1][1]-(indice_after/indice_before)),2)
-        self.SNi = round(self.fi*(self.matrice_transfert[0][0]-(indice_before/indice_after)),2)
-        self.EFo = round(self.fo*self.matrice_transfert[1][1],2)
-        self.SFi = round(self.fi*self.matrice_transfert[0][0],2)
+        self.fo = -indice_before/(-self.matrice_transfert[1][0])
+        self.fi = indice_after/(-self.matrice_transfert[1][0])
+        self.SHi = self.fi*(self.matrice_transfert[0][0]-1)
+        self.EHo = self.fo*(self.matrice_transfert[1][1]-1)
+        self.ENo = self.fo*(self.matrice_transfert[1][1]-(indice_after/indice_before))
+        self.SNi = self.fi*(self.matrice_transfert[0][0]-(indice_before/indice_after))
+        self.EFo = self.fo*self.matrice_transfert[1][1]
+        self.SFi = self.fi*self.matrice_transfert[0][0]
+
+
         print("")
         print("Matrice transfert : ")
         print(self.matrice_transfert[0])
         print(self.matrice_transfert[1])
-        print('fo : {}, fi : {}'.format(self.fo, self.fi))
-        print("EHo : {}".format(self.EHo))
-        print("ENo : {}".format(self.ENo))
-        print("SHi : {}".format(self.SHi))
-        print("SNi : {}".format(self.SNi))
-        print("HoNo : {}, HiNi : {},   fo+fi={}".format(-self.EHo+self.ENo,-self.SHi+self.SNi,self.fo+self.fi))
-        print("EFo : {}, SFi : {}".format(self.fo*self.matrice_transfert[1][1],self.fi*self.matrice_transfert[0][0]))
+        print('fo : {}'.format(round(self.fo, 3)))
+        print('fi : {}'.format(round(self.fi, 3)))
+        print("EHo : {}".format(round(self.EHo, 3)))
+        print("ENo : {}".format(round(self.ENo, 3)))
+        print("SHi : {}".format(round(self.SHi, 3)))
+        print("SNi : {}".format(round(self.SNi, 3)))
+        print("HoNo : {},     HiNi : {},   fo+fi={}".format(round(-self.EHo+self.ENo, 3),round(-self.SHi+self.SNi, 3),round(self.fo+self.fi, 3)))
+        print("EFo : {}".format(round(self.EFo, 3)))
+        print("SFi : {}".format(round(self.SFi, 3)))
 
+        tempo = True
+        while tempo :
+            suite = input("Souhaitez vous utiliser un point objet ? y/n (1/0) -> ")
+            if suite == "y" or suite =="1":
+                tempo = False
+                self.calc_and_print_image()
+            elif suite == "n" or suite =="0":
+                tempo = False
+                print("non")
+            else : 
+                print("Entre une valeur correcte")
+    
+    def calc_and_print_image(self):
+        self.EAo = float(input("Quel est la distance EAo ? : "))
+
+        self.po = - self.EHo + self.EAo
+        self.pi = (self.po*self.list_of_indice[-1])/(-self.matrice_transfert[1][0]*self.po+self.list_of_indice[0])
+        self.Gt = self.pi/self.po
+
+        # T(HiAi) T(HoHi) T(AoHo)
+        self.list_of_matrice_2 = [[[1, self.pi],[0, 1]],[[1,0],[self.matrice_transfert[1][0], 1]],[[1, -self.po],[0, 1]]]
+        for i in range(len(self.list_of_matrice_2)-1):
+            self.list_of_matrice_2[0] = self.multiply_matrice_two_by_two(self.list_of_matrice_2[0], self.list_of_matrice_2[i+1])
+        self.Matrice_AoAi = self.list_of_matrice_2[0]
+
+        for i in range(len(self.Matrice_AoAi)):
+            for j in range(len(self.Matrice_AoAi)):
+                self.Matrice_AoAi[i][j] = round(self.Matrice_AoAi[i][j],3)
+
+        print("-------------------------------")
+        print("po : {},  pi : {}".format(round(self.po,3), round(self.pi,3)))
+        print("Gt (pi/po): {}".format(round(self.Gt,3)))
+        print("Matrice AoAi : ")
+        print(self.Matrice_AoAi[0])
+        print(self.Matrice_AoAi[1])
 a = systemecentree()
 a.start()
 a.calc_and_print_result()
